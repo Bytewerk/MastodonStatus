4c8d49d828343aaf08f252cff9ffe20df7933072
Hubert Denkmair <hubert@denkmair.de>
2018-08-29 23:41:55 +0200

    use fixed field values because mastodon transforms them to html

--- update_open_status.py
+++ update_open_status.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import requests
 
 INSTANCE_URL = 'https://chaos.social'
-UPDATE_FIELD_NAME = 'Status'
 
 try:
 	json = requests.get('http://stats.bytewerk.org/status.json').json()
@@ -13,12 +12,11 @@ except:
 	state = 'unknown'
 
 mastodon = Mastodon(access_token='usersecret.secret', api_base_url=INSTANCE_URL)
-new_fields = []
-for field in mastodon.account_verify_credentials().fields:
-	if field['name'] == UPDATE_FIELD_NAME:
-		new_fields.append((UPDATE_FIELD_NAME, state))
-	else:
-		new_fields.append((field['name'], field['value']))
-
-mastodon.account_update_credentials(fields=new_fields)
+fields = [
+	('Location', 'Krumenauerstr. 54 / Ingolstadt / Germany'),
+	('Visit us', 'Wednesday, 19:00+ (and most other evenings)'),
+	('IRC', '#bytewerk @ freenode'),
+	('Status', state)
+]
+mastodon.account_update_credentials(fields=fields)
 
